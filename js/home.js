bf846e771b3c85dc6412644414e3ab510055a915
thesleepiestdev <sophacles1@outlook.com>
2024-07-28 21:59:44 +0200

    v0.0.2 site

--- js/home.js
+++ js/home.js
@@ -3,35 +3,62 @@ const navbarLinks = document.querySelectorAll('.links .nav-link');
 
 let previousLinkId;
 
+// Function to update the iframe source and active link
+function updateContent(linkId) {
+  const newSrc = getNewIframeSrc(linkId);
+  iframe.src = newSrc;
+
+  navbarLinks.forEach((navLink) => {
+    navLink.classList.remove('activepage');
+  });
+
+  const activeLink = document.getElementById(linkId);
+  if (activeLink) {
+    activeLink.classList.add('activepage');
+  }
+
+  previousLinkId = linkId;
+}
+
+// Function to get the iframe source based on the link ID
+function getNewIframeSrc(linkId) {
+  switch (linkId) {
+    case 'link-home':
+      return '/content.html';
+    case 'link-about':
+      return '/about/content.html';
+    case 'link-blog':
+      return '/blog/content.html';
+    case 'link-gallery':
+      return '/gallery/content.html';
+    default:
+      return '/content.html';
+  }
+}
+
+// Function to get the link ID from the URL parameter
+function getLinkIdFromUrl() {
+  const urlParams = new URLSearchParams(window.location.search);
+  const page = urlParams.get('page');
+  return page ? `link-${page}` : 'link-home';
+}
+
+// Update content based on URL parameter on page load
+document.addEventListener('DOMContentLoaded', () => {
+  const linkId = getLinkIdFromUrl();
+  updateContent(linkId);
+});
+
+// Event listeners for navbar links
 navbarLinks.forEach((link) => {
   link.addEventListener('click', (e) => {
     e.preventDefault();
 
-    const newSrc = getNewIframeSrc(link.id);
-    iframe.src = newSrc;
+    const linkId = link.id;
+    updateContent(linkId);
 
-    navbarLinks.forEach((navLink) => {
-      navLink.classList.remove('activepage');
-    });
-
-    if (link.id !== previousLinkId) {
-      link.classList.add('activepage');
-      previousLinkId = link.id;
-    }
+    // Update the URL without reloading the page
+    const newUrl = `${window.location.origin}${window.location.pathname}?page=${linkId.split('-')[1]}`;
+    window.history.pushState({ path: newUrl }, '', newUrl);
   });
 });
-
-function getNewIframeSrc(linkId) {
-  switch (linkId) {
-    case 'link-home':
-      return 'home.html';
-    case 'link-about':
-      return 'about.html';
-    case 'link-blog':
-      return 'blog.html';
-    case 'link-gallery':
-      return 'gallery.html';
-    default:
-      return '';
-  }
-}
